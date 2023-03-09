f982a8b6147425ef0a3aedfe1b018f2b80443609
Osorio3408 <yuliamwow@gmail.com>
2023-03-09 16:20:54 -0500

    se organizo el checkbox

--- app/src/main/java/com/osorio/taller1/MainActivity.kt
+++ app/src/main/java/com/osorio/taller1/MainActivity.kt
@@ -2,6 +2,7 @@ package com.osorio.taller1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import android.widget.*
 
 class MainActivity : AppCompatActivity() {
@@ -19,14 +20,20 @@ class MainActivity : AppCompatActivity() {
         val radioGroup = findViewById<RadioGroup>(R.id.radioGroup)
         val radioBtnId = radioGroup.checkedRadioButtonId
         val radioButton = findViewById<RadioButton>(radioBtnId)
+        val checkBox1:CheckBox = findViewById<CheckBox>(R.id.checkBox1)
+        val checkBox2:CheckBox = findViewById<CheckBox>(R.id.checkBox2)
         val switchButton = findViewById<Switch>(R.id.switch1)
         val toggleButton = findViewById<ToggleButton>(R.id.toggleButton)
         var text = editText.text.toString()
-        var ToggleChecked = toggleButton.isChecked
+        var toggleChecked = toggleButton.isChecked
         var radiotext = radioButton.text.toString()
         var switchChecked = switchButton.isChecked
+        var checkbox1 = checkBox1.isChecked
+        var checkbox2 = checkBox2.isChecked
 
-        Toast.makeText(this, "$text $radiotext $ToggleChecked $switchChecked ", Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "$text \n $radiotext Seleccionado \n $checkbox1 \n $checkbox2 \n $toggleChecked \n $switchChecked \n", Toast.LENGTH_LONG).show()
+
+
 
     }
 }
